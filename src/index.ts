c3c616e2c9b4a5c84d8ffb8e47d72701cde42fb1
gebeto <slavik.nychkalo@gmail.com>
2018-06-16 19:10:10 +0300

    try to load font

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,42 @@
-console.log("Hello world!!!");
+import * as PIXI from 'pixi.js';
+
+import backgroundImage from './assets/post-bg.png';
+import font from './assets/SFUIDisplayBlack.otf';
+
+import * as O from 'opentype.js';
+
+// const app = new PIXI.Application({
+// 	view: <HTMLCanvasElement>document.getElementById("test-canvas"),
+// });
+
+
+// // create a new Sprite from an image path
+// const background = PIXI.Sprite.fromImage(backgroundImage);
+// background.width = app.renderer.width;
+// background.height = app.renderer.height;
+// app.stage.addChild(background);
+
+O.load(font, (e, font) => {
+	const canvas = <HTMLCanvasElement>document.getElementById("test-canvas");
+	const ctx = <CanvasRenderingContext2D>canvas.getContext("2d");
+	console.log(font);
+	if (font) {
+		font.draw(ctx, "Hello", 0, 30, 30)
+	}
+
+	// const t = new PIXI.Text("Hello world!", {
+	// 	fontFamily: "SF UI Display",
+	// });
+	// app.stage.addChild(t);
+});
+
+// const text = new PIXI.Text("Hello world!", {
+// 	fontFamily: "Archive",
+// 	fontSize: 40,
+// });
+// app.stage.addChild(text);
+
+// Listen for animate update
+// app.ticker.add(function(delta) {
+//		 background.rotation += 0.1 * delta;
+// });
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require("path");
 
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+
 const config = {
 	entry: path.resolve(__dirname, "src/index.ts"),
 	output: {
@@ -16,9 +19,32 @@ const config = {
 			{
 				test: /\.ts$/,
 				loader: "awesome-typescript-loader"
+			},
+			{
+				test: /\.html$/,
+				loader: "html-loader",
+			},
+			{
+				test: /\.(jpg|png)$/,
+				loader: "file-loader",
+			},
+			{
+				test: /\.(ttf|otf)$/,
+				loader: "file-loader",
 			}
 		]
-	}
+	},
+
+	devServer: {
+		port: 3000,
+	},
+
+	plugins: [
+		new HtmlWebPackPlugin({
+			template: "./src/index.html",
+			filename: "./index.html",
+		}),
+	]
 };
 
 module.exports = config;
